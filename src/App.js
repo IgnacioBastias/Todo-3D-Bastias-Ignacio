d3a3896af9ce43887c22c72cbbe6532a5d10d403
IgnacioBastias <103468121+IgnacioBastias@users.noreply.github.com>
2022-08-10 21:42:59 +0200

    ItemDetail

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './App.css';
 import NavBar from "./components/NavBar/NavBar"
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+// import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer/>
+      {/* <ItemListContainer/> */}
+      <ItemDetailContainer />
     </div>
   );
 }
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import itemsData from "../data/itemsData";
+import ItemData from "../data/itemsData";
+import ItemDetail from "./ItemDetail";
+
+function traerItem() {
+    return new Promise((resolve) => {
+
+        setTimeout(() =>
+            resolve(ItemData[2]), 2000
+        );
+    });
+}
+
+const ItemDetailContainer = () => {
+
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+        traerItem()
+            .then((respuesta) => {
+                setItem(respuesta);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    return (
+        <ItemDetail 
+        tipo={item.tipo}
+        titulo={item.titulo}
+        precio={item.precio}
+        img={item.img}
+        stk={item.stk}
+        int={item.int}
+        />
+    )
+};
+
+export default ItemDetailContainer;
