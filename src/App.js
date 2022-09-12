72f8066ce104b95a8303e85e70b614ca9c3bbeed
IgnacioBastias <103468121+IgnacioBastias@users.noreply.github.com>
2022-09-12 07:53:17 -0300

    readme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import './App.css';
 import NavBar from "./components/NavBar/NavBar"
-// import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-// import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
+
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./store/cartContext";
-// import CartView from './components/cartView/CartView';
-// import UserForm from './components/UserForm/UserForm';
 import AnimatedRoutes from './components/animatedRoutes';
 
 
